ad898251141250726c345a699fa640c5ebdbe73f
Nikolaev1ch <vsharanov@yandex.ru>
2024-01-28 12:56:58 +0500

    add for and foreach

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,7 +1,8 @@
 ﻿int n = 5;
-int[] array = { 2, 10, 7, 3, 9 };
+int[] array = { 2, 4, 7, 3, 9 };
 int i = 0;
 int max = array[0];
+
 while(i < n)
 {
     if (array[i] > max)
@@ -9,5 +10,25 @@ while(i < n)
         max = array[i];
     }
     i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for(int j = 0; j < n; j++)
+{
+       if (array[j] > max)
+    {
+        max = array[j];
+    } 
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+       if (e > max)
+    {
+        max = e;
+    } 
 }
 Console.WriteLine(max);
